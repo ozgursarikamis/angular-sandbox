7fd2d3281e843e8aec444be0f64e91807709a12d
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-10-04 22:53:28 +0100

    Profile and NotReady Components

---
+++ src/app/not-ready-yet/not-ready-yet.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-ready-yet',
+  templateUrl: './not-ready-yet.component.html',
+  styleUrls: ['./not-ready-yet.component.css']
+})
+export class NotReadyYetComponent {
+
+}
