429153350aaf0f04d9641ae12b1d3715d65ba1ff
Ozgur Sarikamis <ozgur@techpassport.io>
2023-11-14 01:19:03 +0000

    Observable<Observable<Product>>

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable, catchError, tap, throwError } from 'rxjs';
+import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 import { Product } from './product';
 import { HttpErrorService } from '../utilities/http-error.service';
+import { ReviewService } from '../reviews/review.service';
+import { Review } from '../reviews/review';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +13,7 @@ export class ProductService {
   private productsUrl = 'api/products';
   private http = inject(HttpClient);
   private httpErrorService = inject(HttpErrorService);
+  private reviewService = inject(ReviewService);
 
   constructor(
     // private http: HttpClient
@@ -25,11 +28,23 @@ export class ProductService {
   }
 
   getProduct(id: number): Observable<Product> {
-    return this.http.get<Product>(`${this.productsUrl}s/${id}`)
+    return this.http.get<Product>(`${this.productsUrl}/${id}`)
       .pipe(
         tap(data => console.log(data)),
+        map(product => this.getProductsWithReviews(product)),
         catchError(error => this.handleError(error))
       );
+  }
+
+  private getProductsWithReviews(product: Product): Observable<Product> {
+    if (product.hasReviews) {
+      return this.http.get<Review[]>(this.reviewService.getReviewUrl(product.id))
+        .pipe(
+          map(reviews => ({ ...product, reviews } as Product))
+        );
+    } else {
+      return of(product);
+    }
   }
 
   private handleError(err: any): Observable<never> {
