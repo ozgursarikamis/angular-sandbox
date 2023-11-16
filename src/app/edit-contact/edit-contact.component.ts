2f4db1fb2995d7aaf9b2e40a89a916adb014b9a7
Ozgur Sarikamis <ozgur@techpassport.io>
2023-11-16 21:39:36 +0000

    FormBuilder

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ContactsService } from '../contacts/contacts.service';
+import { Contact } from '../contacts/contact.model';
 
 @Component({
   templateUrl: './edit-contact.component.html',
@@ -9,28 +10,29 @@ import { ContactsService } from '../contacts/contacts.service';
 })
 export class EditContactComponent implements OnInit {
   
-  contactForm = new FormGroup({
-    id: new FormControl(),
-    firstName: new FormControl(),
-    lastName: new FormControl(),
-    dateOfBirth: new FormControl(),
-    favoritesRanking: new FormControl(),
-    address: new FormGroup({
-      streetAddress: new FormControl(),
-      city: new FormControl(),
-      state: new FormControl(),
-      postalCode: new FormControl(),
-      addressType: new FormControl()
-    }),
-    phone: new FormGroup({
-      phoneNumber: new FormControl(),
-      phoneType: new FormControl()
-    }),
-  });
-
   contactService = inject(ContactsService);
   router = inject(Router);
   route = inject(ActivatedRoute);
+  formBuilder = inject(FormBuilder);
+
+  contactForm = this.formBuilder.group({
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: <Date | null>null,
+    favoritesRanking: <number | null>null,
+    address: this.formBuilder.group({
+      streetAddress: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      addressType: ''
+    }),
+    phone: this.formBuilder.group({
+      phoneNumber: '',
+      phoneType: ''
+    }),
+  });
 
   constructor() { }
 
@@ -40,20 +42,20 @@ export class EditContactComponent implements OnInit {
 
     this.contactService.getContact(contactId)
       .subscribe(contact => {
-        this.contactForm.controls.id.setValue(contact?.id);
-        this.contactForm.controls.firstName.setValue(contact?.firstName);
-        this.contactForm.controls.lastName.setValue(contact?.lastName);
-        this.contactForm.controls.dateOfBirth.setValue(contact?.dateOfBirth);
-        this.contactForm.controls.favoritesRanking.setValue(contact?.favoritesRanking);
+        this.contactForm.controls.id.setValue(contact?.id ?? '');
+        this.contactForm.controls.firstName.setValue(contact?.firstName ?? '');
+        this.contactForm.controls.lastName.setValue(contact?.lastName ?? '');
+        this.contactForm.controls.dateOfBirth.setValue(contact?.dateOfBirth ?? null);
+        this.contactForm.controls.favoritesRanking.setValue(contact?.favoritesRanking ?? null);
 
-        this.contactForm.controls.phone.controls.phoneNumber?.setValue(contact?.phone?.phoneNumber);
-        this.contactForm.controls.phone.controls.phoneType?.setValue(contact?.phone?.phoneType);
+        this.contactForm.controls.phone.controls.phoneNumber?.setValue(contact?.phone?.phoneNumber ?? '');
+        this.contactForm.controls.phone.controls.phoneType?.setValue(contact?.phone?.phoneType ?? '');
 
-        this.contactForm.controls.address.controls.streetAddress?.setValue(contact?.address?.streetAddress);
-        this.contactForm.controls.address.controls.city?.setValue(contact?.address?.city);
-        this.contactForm.controls.address.controls.state?.setValue(contact?.address?.state);
-        this.contactForm.controls.address.controls.postalCode?.setValue(contact?.address?.postalCode);
-        this.contactForm.controls.address.controls.addressType?.setValue(contact?.address?.addressType);
+        this.contactForm.controls.address.controls.streetAddress?.setValue(contact?.address?.streetAddress ?? '');
+        this.contactForm.controls.address.controls.city?.setValue(contact?.address?.city ?? '');
+        this.contactForm.controls.address.controls.state?.setValue(contact?.address?.state ?? '');
+        this.contactForm.controls.address.controls.postalCode?.setValue(contact?.address?.postalCode ?? '');
+        this.contactForm.controls.address.controls.addressType?.setValue(contact?.address?.addressType ?? '');
       });
   }
 
@@ -61,7 +63,8 @@ export class EditContactComponent implements OnInit {
     console.log(this.contactForm.value);
     console.log(this.contactForm.getRawValue()); // The aggregate value of the FormGroup, including any disabled controls.
 
-    this.contactService.saveContact(this.contactForm.getRawValue())
+    const rawValue = this.contactForm.getRawValue();
+    this.contactService.saveContact(rawValue as Partial<Contact>)
       .subscribe({
         next: () => this.router.navigate(['/contacts']),
       });
