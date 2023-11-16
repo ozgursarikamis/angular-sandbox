c93999be6fbfd7ee37a4733505d55a6abd56f4bc
Ozgur Sarikamis <ozgur@techpassport.io>
2023-11-16 16:59:53 +0000

    In memory service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { EditContactComponent } from './edit-contact/edit-contact.component';
-import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { InMemoryContactsApi } from './contacts/in-memory-contacts.service';
 
 @NgModule({
   declarations: [
@@ -18,6 +21,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryContactsApi, { delay: 200 }),
     AppRoutingModule
   ],
   providers: [],
