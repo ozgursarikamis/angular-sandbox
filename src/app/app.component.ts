51ec9a26730d59b49e7e7375ea5282cabba69b31
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-11-16 02:11:01 +0000

    Initial Auth0 Config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [AuthService],
 })
 export class AppComponent {
-  title = undefined;
+  title = " Auth0 & Angular";
+  authService = inject(AuthService);
+  constructor(
+    @Inject(DOCUMENT) private doc: Document
+  ) {
+    // document.title = this.title;
+    this.doc.title = this.title;
+  }
+
+  onLoginButtonClick() {
+    this.authService.loginWithRedirect();
+  }
+
+  onLogoutButtonClick() {
+    this.authService.logout({ logoutParams: { returnTo: document.location.origin } });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AuthHttpInterceptor, AuthModule } from '@auth0/auth0-angular';
 import { AppComponent } from './app.component';
+import { environment as env } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AuthModule.forRoot({
+      ...env.auth,
+      httpInterceptor: {
+        ...env.httpInterceptor,
+      },
+    }),
     BrowserModule
   ],
   providers: [],
