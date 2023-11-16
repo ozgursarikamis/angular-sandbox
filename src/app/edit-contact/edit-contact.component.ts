72bddf39541dcfdda9da361bf83d048545175511
Ozgur Sarikamis <ozgur@techpassport.io>
2023-11-16 21:49:15 +0000

    nonNullable

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -15,20 +15,22 @@ export class EditContactComponent implements OnInit {
   route = inject(ActivatedRoute);
   formBuilder = inject(FormBuilder);
 
-  contactForm = this.formBuilder.group({
+  // When constructing a control, it will be non-nullable, 
+  // and will reset to its initial value.
+  contactForm = this.formBuilder.nonNullable.group({
     id: '',
     firstName: '',
     lastName: '',
-    dateOfBirth: <Date | null>null,
+    dateOfBirth: <Date | null>null, // except for dateOfBirth and favoritesRanking
     favoritesRanking: <number | null>null,
-    address: this.formBuilder.group({
+    address: this.formBuilder.nonNullable.group({
       streetAddress: '',
       city: '',
       state: '',
       postalCode: '',
       addressType: ''
     }),
-    phone: this.formBuilder.group({
+    phone: this.formBuilder.nonNullable.group({
       phoneNumber: '',
       phoneType: ''
     }),
@@ -42,20 +44,25 @@ export class EditContactComponent implements OnInit {
 
     this.contactService.getContact(contactId)
       .subscribe(contact => {
-        this.contactForm.controls.id.setValue(contact?.id ?? '');
-        this.contactForm.controls.firstName.setValue(contact?.firstName ?? '');
-        this.contactForm.controls.lastName.setValue(contact?.lastName ?? '');
-        this.contactForm.controls.dateOfBirth.setValue(contact?.dateOfBirth ?? null);
-        this.contactForm.controls.favoritesRanking.setValue(contact?.favoritesRanking ?? null);
 
-        this.contactForm.controls.phone.controls.phoneNumber?.setValue(contact?.phone?.phoneNumber ?? '');
-        this.contactForm.controls.phone.controls.phoneType?.setValue(contact?.phone?.phoneType ?? '');
+         if(!contact) {
+          return;
+         }
 
-        this.contactForm.controls.address.controls.streetAddress?.setValue(contact?.address?.streetAddress ?? '');
-        this.contactForm.controls.address.controls.city?.setValue(contact?.address?.city ?? '');
-        this.contactForm.controls.address.controls.state?.setValue(contact?.address?.state ?? '');
-        this.contactForm.controls.address.controls.postalCode?.setValue(contact?.address?.postalCode ?? '');
-        this.contactForm.controls.address.controls.addressType?.setValue(contact?.address?.addressType ?? '');
+        this.contactForm.controls.id.setValue(contact.id);
+        this.contactForm.controls.firstName.setValue(contact.firstName);
+        this.contactForm.controls.lastName.setValue(contact.lastName);
+        this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth ?? null);
+        this.contactForm.controls.favoritesRanking.setValue(contact.favoritesRanking ?? null);
+
+        this.contactForm.controls.phone.controls.phoneNumber?.setValue(contact.phone.phoneNumber);
+        this.contactForm.controls.phone.controls.phoneType?.setValue(contact.phone.phoneType);
+
+        this.contactForm.controls.address.controls.streetAddress?.setValue(contact.address.streetAddress);
+        this.contactForm.controls.address.controls.city?.setValue(contact.address.city);
+        this.contactForm.controls.address.controls.state?.setValue(contact.address.state);
+        this.contactForm.controls.address.controls.postalCode?.setValue(contact.address.postalCode);
+        this.contactForm.controls.address.controls.addressType?.setValue(contact.address.addressType);
       });
   }
 
