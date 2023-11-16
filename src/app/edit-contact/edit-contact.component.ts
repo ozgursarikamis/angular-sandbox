283bed86ae454e2483afe0a8a5bafbe8ceb6551f
Ozgur Sarikamis <ozgur@techpassport.io>
2023-11-16 22:00:58 +0000

    `patchValue()` method

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -49,20 +49,25 @@ export class EditContactComponent implements OnInit {
           return;
          }
 
-        this.contactForm.controls.id.setValue(contact.id);
-        this.contactForm.controls.firstName.setValue(contact.firstName);
-        this.contactForm.controls.lastName.setValue(contact.lastName);
-        this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth ?? null);
-        this.contactForm.controls.favoritesRanking.setValue(contact.favoritesRanking ?? null);
+        // this.contactForm.controls.id.setValue(contact.id);
+        // this.contactForm.controls.firstName.setValue(contact.firstName);
+        // this.contactForm.controls.lastName.setValue(contact.lastName);
+        // this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth ?? null);
+        // this.contactForm.controls.favoritesRanking.setValue(contact.favoritesRanking ?? null);
 
-        this.contactForm.controls.phone.controls.phoneNumber?.setValue(contact.phone.phoneNumber);
-        this.contactForm.controls.phone.controls.phoneType?.setValue(contact.phone.phoneType);
+        // this.contactForm.controls.phone.controls.phoneNumber?.setValue(contact.phone.phoneNumber);
+        // this.contactForm.controls.phone.controls.phoneType?.setValue(contact.phone.phoneType);
 
-        this.contactForm.controls.address.controls.streetAddress?.setValue(contact.address.streetAddress);
-        this.contactForm.controls.address.controls.city?.setValue(contact.address.city);
-        this.contactForm.controls.address.controls.state?.setValue(contact.address.state);
-        this.contactForm.controls.address.controls.postalCode?.setValue(contact.address.postalCode);
-        this.contactForm.controls.address.controls.addressType?.setValue(contact.address.addressType);
+        // this.contactForm.controls.address.controls.streetAddress?.setValue(contact.address.streetAddress);
+        // this.contactForm.controls.address.controls.city?.setValue(contact.address.city);
+        // this.contactForm.controls.address.controls.state?.setValue(contact.address.state);
+        // this.contactForm.controls.address.controls.postalCode?.setValue(contact.address.postalCode);
+        // this.contactForm.controls.address.controls.addressType?.setValue(contact.address.addressType);
+
+        // what if we didn't initialize the whole form with values: 
+        const names = { firstName: contact.firstName, lastName: contact.lastName };
+        // this.contactForm.setValue(names); // won't work
+        this.contactForm.patchValue(names); // works
       });
   }
 
