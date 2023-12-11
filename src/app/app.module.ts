c6bf9eb9de416166e5760ab8e1898d1476658b46
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-12-11 03:08:25 +0000

    @ngrx/store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { InMemoryDataService } from './in-memory-data.service';
 import { HomeComponent } from './home/home.component';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -17,6 +18,15 @@ import { StoreModule } from '@ngrx/store';
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService),
     StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      name: 'NgRx : Getting Started',
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(),
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+      connectInZone: true // If set to true, the connection is established within the Angular zone
+    })
   ],
   providers: [],
   bootstrap: [AppComponent],
