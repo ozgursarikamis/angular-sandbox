803620ad80a40a42cdaba46ccf23ca98d8f22ac4
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-12-17 00:43:43 +0000

    DI Tokens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Inject, InjectionToken, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FileSizePipe } from './pipes/file-size.pipe';
+
+// Define a new DI Token:
+export const APP_CONFIG = new InjectionToken<AppConfig>('app.config'); // `app.config` is the name of the token
+
+export interface AppConfig {
+  appTitle: string;
+  apiEndpoint: string;
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +20,21 @@ import { FileSizePipe } from './pipes/file-size.pipe';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_CONFIG,
+      useValue: {
+        appTitle: 'Interview Questions',
+        apiEndpoint: 'http://localhost:3000'
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    @Inject(APP_CONFIG) private config: AppConfig
+  ) {
+    console.log(config);
+  }
+}
