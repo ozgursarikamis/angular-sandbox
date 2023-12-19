1584fac87c57722895465f7403c802a53bb289cf
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-12-19 15:04:31 +0000

    Post method in service

--- src/app/calc.service.ts
+++ src/app/calc.service.ts
@@ -1,17 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { ApiUrls } from './strings';
+import { User } from './models/User';
+import { Post } from './models/Post';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CalcService {
   httpClient = inject(HttpClient);
   usersUrl = ApiUrls.Users;
+  postsUrl = ApiUrls.Posts;
 
   multiply(a: number, b: number): number {
     return a * b;
   }
 
-  getUsers(): any {
-    return this.httpClient.get(this.usersUrl);
+  getUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(this.usersUrl);
   }
+
+  savePost(post: Post): Observable<Post> {
+    return this.httpClient.post<Post>(this.postsUrl, post);
+  }
+
 }
--- src/app/strings.ts
+++ src/app/strings.ts
@@ -1,3 +1,4 @@
 export enum ApiUrls {
-    Users = 'https://jsonplaceholder.typicode.com/users',
+  Users = 'https://jsonplaceholder.typicode.com/users',
+  Posts = "https://jsonplaceholder.typicode.com/posts"
 }
