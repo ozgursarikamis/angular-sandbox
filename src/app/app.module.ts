8285b83e47416992c05621802c87962ce9aeece0
Ozgur Sarikamis <ozgur@techpassport.io>
2024-03-18 14:41:08 +0000

    PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent }            from './app.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule }            from "@angular/router";
+
+import { NodeService }             from "./services/node.service";
+
+import { FormsModule }             from "@angular/forms";
+import { SplitButtonModule }       from "primeng/splitbutton";
+import { MessageService }          from "primeng/api";
+import { FloatLabelModule }        from "primeng/floatlabel";
+import { InputTextModule }         from "primeng/inputtext";
+import { EditorModule }            from "primeng/editor";
+import { TreeSelectModule }        from "primeng/treeselect";
+import { TabViewModule }           from "primeng/tabview";
+import { FieldsetModule }          from "primeng/fieldset";
+import { MenubarModule }           from "primeng/menubar";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    SplitButtonModule,
+    FloatLabelModule,
+    FormsModule,
+    InputTextModule,
+    EditorModule,
+    TreeSelectModule,
+    TabViewModule,
+    FieldsetModule,
+    MenubarModule
   ],
-  providers: [],
+  providers: [
+    MessageService,
+    NodeService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
