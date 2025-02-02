5a8056296e1ab24cfbeb0fda82c4fe808b824d1d
Ozgur Sarikamis <ozgur.sarikamis@gmail.com>
2025-02-02 16:36:51 +0300

    AfterViewChecked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, DoCheck, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+// ==== Why Use ngAfterViewChecked() Here? ====
+// Every time Angular adds a new message, the view updates.
+// The hook detects these updates and scrolls to the bottom only when needed.
+// ngAfterViewInit() wouldn’t work because the messages change after the view is initialized.
+
+// It gets called after every change detection cycle, so you must optimize it to avoid performance issues 
+// (e.g., avoid making API calls or triggering new change detections).
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = undefined;
+export class AppComponent implements AfterViewChecked, OnInit, DoCheck {
+  title = 'ngAfterViewChecked';
+  @ViewChild('messagesContainer') messagesContainer!: ElementRef;
+  private previousMessageCount = 0;
+  messages: string[] = ['Hello!', 'How are you?', 'I am fine!'];
+
+  ngOnInit(): void {
+    console.log(this.messagesContainer);
+    this.styleContainer();
+  }
+  ngDoCheck(): void {
+    console.log('DoCheck');
+  }
+  
+  ngAfterViewChecked(): void {
+    console.log(this.messagesContainer);
+    this.styleContainer();
+    if (this.messages.length !== this.previousMessageCount) {
+      this.previousMessageCount = this.messages.length;
+      this.scrollToBottom();
+      this.previousMessageCount = this.messages.length;
+    }
+  }
+  
+  scrollToBottom() {
+    const container = this.messagesContainer.nativeElement;
+    container.scrollTop = container.scrollHeight;
+  }
+
+  addMessage(message: string) {
+    this.messages.push(message);
+  }
+
+  styleContainer() {
+    this.messagesContainer.nativeElement.style.border = '1px solid red';
+  }
 }
