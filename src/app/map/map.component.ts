ad9ad262518a1756d06b5399dabff72679b68362
Ozgur Sarikamis <ozgur.sarikamis@gmail.com>
2025-05-09 23:14:01 +0300

    Turf, event handler for draw

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -3,6 +3,8 @@ import { GeolocateControl, LngLatLike, Map, MapOptions, Marker, NavigationContro
 import { environment } from 'src/environments/environment';
 
 import MapboxDraw from "@mapbox/mapbox-gl-draw";
+import * as turf from '@turf/turf';
+import { FeatureCollection } from 'geojson';
 
 const CENTER_COORDINATES = [-2.40, 54.455] as LngLatLike
 
@@ -19,12 +21,14 @@ export class MapComponent implements AfterViewInit {
   width!: string;
   style = 'mapbox://styles/mapbox/outdoors-v12';
   zoomToMarker: Marker | undefined;
+  Draw: MapboxDraw = new MapboxDraw();
 
   constructor() {
     this.map?.remove();
     this.mapContainer?.nativeElement.removeChild(this);
   }
-    createMap() {
+
+  createMap() {
     const center = CENTER_COORDINATES as LngLatLike;
     const projection = 'globe' as unknown as ProjectionSpecification;
 
@@ -44,13 +48,33 @@ export class MapComponent implements AfterViewInit {
       // this.add3DBuildings();
     });
 
-    var Draw = new MapboxDraw();
-    this.map.addControl(Draw, 'top-left');
+    this.map.addControl(this.Draw!, 'top-left');
+
+    const calculateArea = (e: any) => {
+      const data: FeatureCollection = this.Draw!.getAll();
+      const answer = document.getElementById('calculated-area');
+      console.log('data', data);
+
+      if (data.features.length > 0) {
+        const area = turf.area(data);
+        // Restrict the area to 2 decimal points.
+        const rounded_area = Math.round(area * 100) / 100;
+        answer!.innerHTML = `<strong>${rounded_area}</strong> square meters`;
+      } else {
+        answer!.innerHTML = '';
+        if (e.type !== 'draw.delete')
+          alert('Click the map to draw a polygon.');
+      }
+    }
+
+    this.map.on('draw.create', calculateArea);
+    this.map.on('draw.delete', calculateArea);
+    this.map.on('draw.update', calculateArea);
 
     return this.map;
   }
 
-    private addControls() {
+  private addControls() {
     const navControl = new NavigationControl({ showCompass: true, showZoom: true });
     this.map.addControl(navControl, 'bottom-left');
 
