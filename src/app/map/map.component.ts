7d459df37a27da975ee94c64e0694a753c5387aa
Ozgur Sarikamis <ozgur.sarikamis@gmail.com>
2025-08-13 13:59:31 +0300

    Maps with styles

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import maplibregl from 'maplibre-gl';
+import { environment } from 'src/environment/environment';
+
+const MAPTILER_KEY = environment.mapTilerKey;
 
 @Component({
   selector: 'app-map',
@@ -6,6 +10,23 @@ import { Component } from '@angular/core';
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
-export class MapComponent {
+export class MapComponent implements AfterViewInit {
+  @ViewChild('mapContainer') mapContainer!: ElementRef;
 
+  ngAfterViewInit(): void {
+    const map = new maplibregl.Map({
+      container: this.mapContainer.nativeElement,
+      style: `https://api.maptiler.com/maps/streets/style.json?key=${MAPTILER_KEY}`,
+      center: [0, 0],
+      zoom: 0
+    });
+    console.log({ map });
+  }
 }
+
+/*
+// Replace {api_key} with your Maptiler key
+  style: `https://api.maptiler.com/maps/streets/style.json?key={api_key}`
+  style: `https://api.maptiler.com/maps/outdoor/style.json?key={api_key}`
+  style: `https://api.maptiler.com/maps/basic/style.json?key={api_key}`
+*/
