15bf20b7efaada6e5c96433dc371cd01cdf4528e
Ozgur Sarikamis <ozgur.sarikamis@gmail.com>
2025-09-24 02:30:19 +0300

    CountStore in Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CountStore } from 'src/store/CountStore';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    standalone: false
+    standalone: false,
+    // Provide the store for this component and its children
+    providers: [CountStore]
 })
 export class AppComponent {
   title = undefined;
+
+  readonly countStore = inject(CountStore);
 }
