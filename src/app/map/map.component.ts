25cf94b928ec6188e98a3fabbe1a0a5566be609a
Ozgur Sarikamis <ozgur.sarikamis@gmail.com>
2025-09-26 20:30:41 +0300

    Mapbox maps basics

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,10 +1,9 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { GeolocateControl, LngLatLike, Map, MapOptions, Marker, NavigationControl, ProjectionSpecification } from 'mapbox-gl';
+import {
+  GeolocateControl, LngLatLike, Map, MapOptions, NavigationControl, ProjectionSpecification,
+  FullscreenControl
+} from 'mapbox-gl';
 import { environment } from 'src/environments/environment';
-
-import MapboxDraw from "@mapbox/mapbox-gl-draw";
-import * as turf from '@turf/turf';
-import { FeatureCollection } from 'geojson';
 
 const CENTER_COORDINATES = [-2.40, 54.455] as LngLatLike
 
@@ -19,18 +18,6 @@ export class MapComponent implements AfterViewInit {
 
   map!: Map;
   width!: string;
-  style = 'mapbox://styles/mapbox/outdoors-v12';
-  zoomToMarker: Marker | undefined;
-   Draw: MapboxDraw = new MapboxDraw({
-    controls: {
-      point: false,
-      line_string: false,
-      polygon: true,
-      trash: true,
-      combine_features: false,
-      uncombine_features: false
-    }
-  });
 
   constructor() {
     this.map?.remove();
@@ -44,54 +31,35 @@ export class MapComponent implements AfterViewInit {
     const mapOptions: MapOptions = {
       accessToken: environment.MapboxAccessToken,
       container: this.mapContainer.nativeElement,
-      style: this.style,
+      style: 'mapbox://styles/mapbox/outdoors-v12',
       zoom: 6,
       center,
-      projection: projection,
-      attributionControl: false
+      projection,
+      attributionControl: false,
+      maxPitch: 60,
+      hash: true
     };
     this.map = new Map(mapOptions);
     this.addControls();
 
     this.map.on('style.load', () => {
-      // this.add3DBuildings();
+      console.log('style loaded');
     });
-
-    this.map.addControl(this.Draw!, 'top-left');
-
-    const calculateArea = (e: any) => {
-      const data: FeatureCollection = this.Draw!.getAll();
-      const answer = document.getElementById('calculated-area');
-      console.log('data', data);
-
-      if (data.features.length > 0) {
-        const area = turf.area(data);
-        // Restrict the area to 2 decimal points.
-        const rounded_area = Math.round(area * 100) / 100;
-        answer!.innerHTML = `<strong>${rounded_area}</strong> square meters`;
-      } else {
-        answer!.innerHTML = '';
-        if (e.type !== 'draw.delete')
-          alert('Click the map to draw a polygon.');
-      }
-    }
-
-    this.map.on('draw.create', calculateArea);
-    this.map.on('draw.delete', calculateArea);
-    this.map.on('draw.update', calculateArea);
 
     return this.map;
   }
 
   private addControls() {
     const navControl = new NavigationControl({ showCompass: true, showZoom: true });
-    this.map.addControl(navControl, 'bottom-left');
+    const controlPosition = 'bottom-right';
+    this.map.addControl(navControl, controlPosition);
 
     const geoLocateControl = new GeolocateControl({
       positionOptions: { enableHighAccuracy: true },
       trackUserLocation: true
     });
-    this.map.addControl(geoLocateControl, 'top-right');
+    this.map.addControl(geoLocateControl, controlPosition);
+    this.map.addControl(new FullscreenControl(), controlPosition);
   }
 
   ngAfterViewInit() {
