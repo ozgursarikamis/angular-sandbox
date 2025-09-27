56e115c69f0363845c3b8bf1d57174c97dcde96a
Ozgur Sarikamis <ozgur.sarikamis@gmail.com>
2025-09-27 03:05:55 +0300

    MVT Layer

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,11 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import {
   GeolocateControl, LngLatLike, Map, MapOptions, NavigationControl, ProjectionSpecification,
-  FullscreenControl
+  FullscreenControl,
+  SourceSpecification,
+  LayerSpecification,
+  MapMouseEvent,
+  Popup
 } from 'mapbox-gl';
 import { environment } from 'src/environments/environment';
 
@@ -18,6 +22,7 @@ export class MapComponent implements AfterViewInit {
 
   map!: Map;
   width!: string;
+  popup: Popup | undefined;
 
   constructor() {
     this.map?.remove();
@@ -43,6 +48,54 @@ export class MapComponent implements AfterViewInit {
 
     this.map.on('style.load', () => {
       console.log('style loaded');
+    });
+
+    this.map.on('load', idleListener => {
+      const { type, target } = idleListener;
+
+      // add polygon MVT layer:
+      const source = {
+        type: 'vector',
+        tiles: ['http://localhost:5000/api/polygon/regions/{z}/{x}/{y}.pbf']
+      } as SourceSpecification
+      this.map.addSource('regionSource', source);
+
+      const layer = {
+        id: 'regions_layer',
+        type: 'fill',
+        source: 'regionSource',
+        'source-layer': 'source_layer_regions',
+        'layout': {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        'paint': {
+          'fill-color': 'rgba(0, 0, 0, 0.4)',
+        },
+        slot: 'middle' // middle slot in Mapbox Standard style
+      } as LayerSpecification;
+
+      this.map.addLayer(layer)
+        .on('mouseenter', 'regions_layer', (e: MapMouseEvent) => {
+          // console.log(e);
+          const {
+            features, type, target, point,
+            lngLat
+          } = e;
+          const properties = features?.at(0)?.properties;
+
+          const Name = properties!["Name"];
+          const Id = properties!["Id"];
+
+          this.popup = new Popup({ offset: 1, anchor: 'top', closeButton: false, closeOnMove: true });
+          this.popup.setLngLat(lngLat);
+          this.popup.setHTML(`<h3>${Id} - ${Name}</h3>`);
+
+          this.popup.addTo(this.map);
+        })
+        .on('mouseleave', 'regions_layer', (e: any) => {
+          this.popup?.remove();
+        });
     });
 
     return this.map;
