01186c693e245a37a9d0c88c028d59b76b022f57
Ozgur Sarikamis <ozgur.sarikamis@gmail.com>
2025-09-28 11:49:19 +0300

    Layer bound updated

--- src/app/layers.config.ts
+++ src/app/layers.config.ts
@@ -3,7 +3,7 @@ import { LayerSpecification, VectorSourceSpecification } from "mapbox-gl";
 export const REGIONS_SOURCE: VectorSourceSpecification = {
   type: 'vector',
   tiles: ['http://localhost:5000/api/polygon/regions/{z}/{x}/{y}.pbf'],
-  bounds: [-6.41866722959945,49.864668189761,1.76370560966352,55.8110853652179] // Hard-coded
+  bounds: [-6.41866722959945,49.864668189761,1.76370560966352,55.8110853652179]
 };
 
 export const REGIONS_LAYER: LayerSpecification = {
