432d580adad031e97d376d905fa1c17272a2f440
Ozgur Sarikamis <ozgur.sarikamis@gmail.com>
2025-09-28 03:02:25 +0300

    Hard-coded in `bounds` in source layer

--- src/app/layers.config.ts
+++ src/app/layers.config.ts
@@ -1,8 +1,9 @@
-import { LayerSpecification, SourceSpecification } from "mapbox-gl";
+import { LayerSpecification, VectorSourceSpecification } from "mapbox-gl";
 
-export const REGIONS_SOURCE: SourceSpecification = {
+export const REGIONS_SOURCE: VectorSourceSpecification = {
   type: 'vector',
-  tiles: ['http://localhost:5000/api/polygon/regions/{z}/{x}/{y}.pbf']
+  tiles: ['http://localhost:5000/api/polygon/regions/{z}/{x}/{y}.pbf'],
+  bounds: [-6.41866722959945,49.864668189761,1.76370560966352,55.8110853652179] // Hard-coded
 };
 
 export const REGIONS_LAYER: LayerSpecification = {
@@ -16,7 +17,7 @@ export const REGIONS_LAYER: LayerSpecification = {
   slot: 'middle'
 };
 
-export const BORDER_LAYER: LayerSpecification = {
+export const REGIONS_BORDER_LAYER: LayerSpecification = {
   id: 'regions_layer_borders',
   type: 'line',
   source: REGIONS_LAYER.source,
