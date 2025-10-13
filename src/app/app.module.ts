7f37e3fb272743cb4a9859a5b69d756722e8c093
Ozgur Sarikamis <ozgur.sarikamis@gmail.com>
2025-10-14 00:46:52 +0300

    Practice 1 Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PracticeOneComponent } from "./practice-one/practice-one.component";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PracticeOneComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/practice-one/practice-one.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practice-one',
+  imports: [],
+  templateUrl: './practice-one.component.html',
+  styleUrl: './practice-one.component.scss'
+})
+export class PracticeOneComponent {
+
+}
